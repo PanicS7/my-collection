166c9fc22f7d48f8610f3e7e348785eb9c2b2a47
Sanja Panic <gamesg274@gmail.com>
2023-09-12 23:48:21 +0200

    add item counter

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -2,7 +2,7 @@ import { useAuth } from "../hooks/useAuth";
 import useFirestore from "../hooks/useFirestore";
 
 const ImageGallery = () => {
-  const { docs: images, isLoading } = useFirestore("images");
+  const { docs: images, isLoading, itemCount } = useFirestore("images");
   const { user } = useAuth();
 
   if (isLoading) {
@@ -14,32 +14,40 @@ const ImageGallery = () => {
   }
 
   return (
-    <div className="grid md:grid-cols-3 justify-center gap-3 my-10">
-      {images.map((image) => {
-        if (user.email === image.email) {
-          return (
-            <div className="bg-red-500 rounded-xl" key={image.imageUrl}>
-              <div className="p-3">
-                <div className="mb-2 flex flex-col items-center justify-between">
-                  <p className="block font-sans text-white font-medium leading-relaxed text-blue-gray-900 antialiased">
-                    {image.category}
-                  </p>
-                  <p className="block font-sans text-white font-medium leading-relaxed text-blue-gray-900 antialiased">
-                    {image.title}
-                  </p>
+    <div>
+      <div className="stats shadow">
+        <div className="stat">
+          <div className="stat-title">Total Items Count</div>
+          <div className="stat-value">{itemCount}</div>
+        </div>
+      </div>
+      <div className="grid md:grid-cols-3 justify-center gap-3 my-10">
+        {images.map((image) => {
+          if (user.email === image.email) {
+            return (
+              <div className="bg-red-500 rounded-xl" key={image.imageUrl}>
+                <div className="p-3">
+                  <div className="mb-2 flex flex-col items-center justify-between">
+                    <p className="block font-sans text-white font-medium leading-relaxed text-blue-gray-900 antialiased">
+                      {image.category}
+                    </p>
+                    <p className="block font-sans text-white font-medium leading-relaxed text-blue-gray-900 antialiased">
+                      {image.title}
+                    </p>
+                  </div>
+                </div>
+                <div className="relative h-96 overflow-hidden bg-white bg-clip-border text-gray-700">
+                  <img
+                    src={image.imageUrl}
+                    alt={image.title}
+                    className="h-full w-full object-cover"
+                  />
                 </div>
               </div>
-              <div className="relative h-96 overflow-hidden bg-white bg-clip-border text-gray-700">
-                <img
-                  src={image.imageUrl}
-                  alt={image.title}
-                  className="h-full w-full object-cover"
-                />
-              </div>
-            </div>
-          );
-        }
-      })}
+            );
+          }
+        })}
+      </div>
     </div>
   );
 };
--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,10 +1,13 @@
-import { collection, onSnapshot, query } from "firebase/firestore";
+import { collection, getCountFromServer, onSnapshot, query, where } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db } from "../firebase/config";
+import { useAuth } from "../hooks/useAuth";
 
 const useFirestore = (collectionName) => {
   const [docs, setDocs] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [itemCount, setItemCount] = useState(0);
+  const { user } = useAuth();
 
   useEffect(() => {
     let unsubscribe = () => null
@@ -31,12 +34,20 @@ const useFirestore = (collectionName) => {
       }
     };
 
+    const getItemCount = async () => {
+      const coll = collection(db, collectionName);
+      const q = query(coll, where("userEmail", "==", user.email));
+      const snapshot = await getCountFromServer(q);
+      setItemCount(snapshot.data().count)
+    }
+
     getData()
+    getItemCount()
 
     return () => unsubscribe && unsubscribe()
-  }, [collectionName]);
+  }, [collectionName, user.email]);
 
-  return { docs, isLoading };
+  return { docs, isLoading, itemCount };
 };
 
 export default useFirestore;
