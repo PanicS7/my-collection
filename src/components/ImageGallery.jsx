a56cdf40cbf30c0c5400a4118d8a7d72df14a7c4
Sanja Panic <gamesg274@gmail.com>
2023-09-13 21:15:12 +0200

    add show/hide collection items
    reduce storage image url call, less bandwidth

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import useFirestore from "../hooks/useFirestore";
 
 const ImageGallery = () => {
   const { docs: images, isLoading } = useFirestore("images");
+  const [show, setShow] = useState(false);
 
   if (isLoading) {
     return (
@@ -13,36 +15,53 @@ const ImageGallery = () => {
 
   return (
     <div>
-      <div className="stats shadow">
-        <div className="stat">
-          <div className="stat-title">Total Items Count</div>
-          <div className="stat-value">{images.length}</div>
+      <div className="collapse bg-base-200 mt-10">
+        <input
+          type="checkbox"
+          value={show}
+          onChange={() => setShow(!show)}
+          className="appearance-none"
+        />
+        <div className="collapse-title text-xl font-medium">
+          {show ? "Hide collection" : "Show collection"}
         </div>
-      </div>
-      <div className="grid md:grid-cols-3 justify-center gap-3 my-10">
-        {images.map((image) => {
-            return (
-              <div className="bg-red-500 rounded-xl" key={image.imageUrl}>
-                <div className="p-3">
-                  <div className="mb-2 flex flex-col items-center justify-between">
-                    <p className="block font-sans text-white font-medium leading-relaxed text-blue-gray-900 antialiased">
-                      {image.category}
-                    </p>
-                    <p className="block font-sans text-white font-medium leading-relaxed text-blue-gray-900 antialiased">
-                      {image.title}
-                    </p>
-                  </div>
-                </div>
-                <div className="relative h-96 overflow-hidden bg-white bg-clip-border text-gray-700">
-                  <img
-                    src={image.imageUrl}
-                    alt={image.title}
-                    className="h-full w-full object-cover"
-                  />
+        <div className="collapse-content">
+          {show && (
+            <div>
+              <div className="stats shadow">
+                <div className="stat">
+                  <div className="stat-title">Total Items Count</div>
+                  <div className="stat-value">{images.length}</div>
                 </div>
               </div>
-            );
-        })}
+              <div className="grid md:grid-cols-3 justify-center gap-3 my-10">
+                {images.map((image) => {
+                  return (
+                    <div className="bg-red-500 rounded-xl" key={image.imageUrl}>
+                      <div className="p-3">
+                        <div className="mb-2 flex flex-col items-center justify-between">
+                          <p className="block font-sans text-white font-medium leading-relaxed text-blue-gray-900 antialiased">
+                            {image.category}
+                          </p>
+                          <p className="block font-sans text-white font-medium leading-relaxed text-blue-gray-900 antialiased">
+                            {image.title}
+                          </p>
+                        </div>
+                      </div>
+                      <div className="relative h-96 overflow-hidden bg-white bg-clip-border text-gray-700">
+                        <img
+                          src={image.imageUrl}
+                          alt={image.title}
+                          className="h-full w-full object-cover"
+                        />
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
